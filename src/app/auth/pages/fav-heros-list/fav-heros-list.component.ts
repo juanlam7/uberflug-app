946a44f9950fa5cbd3dc5028fc1850a876817340
juanlam7 <juanlam7@gmail.com>
2021-06-20 21:23:43 -0400

    Vista de favoritos

--- src/app/auth/pages/fav-heros-list/fav-heros-list.component.ts
+++ src/app/auth/pages/fav-heros-list/fav-heros-list.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavHerosListComponent implements OnInit {
 
+  isLoading: boolean = false;
+
+  allCharacters: any;
+  page!: number;
+  results: number = 12;
+  diffOrder: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    let exist = JSON.parse(localStorage.getItem('Favoritos')!);
+    if(exist === null) {
+      console.log('No hay favoritos');
+      
+    } else {
+      this.allCharacters = exist;
+      this.results = this.allCharacters.length;
+    }
   }
 
+  changeOrder() {
+    this.diffOrder === false ? this.diffOrder = true : this.diffOrder = false;
+    this.allCharacters.sort(this.sortFunc);
+  }
+
+  sortFunc (a: any, b: any) {
+    return b === a ? 0 : b < a ? 1 : -1;
+  }
 }
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import {MatButtonModule} from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { Routes, RouterModule  } from '@angular/router';
 
 import { TopToolbarComponent } from './top-toolbar/top-toolbar.component';
 import { CardComponent } from './card/card.component';
@@ -15,7 +16,8 @@ import { CardComponent } from './card/card.component';
   imports: [
     CommonModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    RouterModule
   ],
   exports: [
     TopToolbarComponent,
