2a95331517c7bf14ce5748565957b7f341ade36d
juanlam7 <juanlam7@gmail.com>
2024-10-15 18:13:42 +0200

    feat: Refactor heros list page

--- src/app/pages/heros-list/heros-list.component.ts
+++ src/app/pages/heros-list/heros-list.component.ts
@@ -1,37 +1,48 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, effect, inject, signal } from '@angular/core';
-import { NgxPaginationModule } from 'ngx-pagination';
+import { CommonModule, JsonPipe } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  inject,
+  signal,
+} from '@angular/core';
 
 import { CharactersService } from 'src/app/services/characters.service';
 
 import { Character } from 'src/app/types/characters';
 
 import { CardComponent } from 'src/app/components/card/card.component';
+import { ScrollNearEndDirective } from 'src/app/utils/directives/scroll-near-end.directive';
 
 @Component({
   selector: 'app-heros-list',
   standalone: true,
   templateUrl: './heros-list.component.html',
   styleUrls: ['./heros-list.component.scss'],
-  imports: [
-    NgxPaginationModule,
-    CommonModule,
-    CardComponent,
-  ],
+  imports: [CommonModule, CardComponent, ScrollNearEndDirective, JsonPipe],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HerosListComponent {
-
-  charactersService = inject(CharactersService)
+  charactersService = inject(CharactersService);
 
   allCharacters = signal<Character[] | null>(null);
 
+  limit = signal<number>(12);
+  offset = signal<number>(0);
+
   constructor() {
-    effect((cleanUp) => {
-      const subscription = this.charactersService.getAllCharacters()
-        .subscribe((p) => this.allCharacters.set(p))
+    effect(cleanUp => {
+      const subscription = this.charactersService
+        .getAllCharacters(this.limit(), this.offset())
+        .subscribe(p => {
+          this.allCharacters.update(val => (val ? [...val, ...p] : p));
+        });
 
       cleanUp(() => subscription.unsubscribe());
     });
+  }
+
+  onNearEndScroll(): void {
+    this.offset.update(oldvalue => oldvalue + 12);
   }
 }
--- src/app/services/characters.service.ts
+++ src/app/services/characters.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { catchError, map, Observable, retry, throwError } from 'rxjs';
+import { catchError, debounceTime, map, Observable, retry, throwError } from 'rxjs';
 import { allCharactersResponse, Character } from '../types/characters';
 import { allComicResponse, Comic } from '../types/comics';
 
@@ -13,9 +13,9 @@ export class CharactersService {
   http = inject(HttpClient);
 
   // TODO: add pagination to avoid big load of data who slow down the app performance
-  getAllCharacters(): Observable<Character[]> {
-    const endpoint = `${BASE_API}?ts=1&apikey=e5dc9090bc2546ae10b3abe84382751c&hash=05a1400f79b7e0a15a8ea3d74e8d1f1a&limit=12`;
-    return this.http.get<allCharactersResponse>(endpoint).pipe(retry(1),
+  getAllCharacters(limit: number, offset: number): Observable<Character[]> {
+    const endpoint = `${BASE_API}?ts=1&apikey=e5dc9090bc2546ae10b3abe84382751c&hash=05a1400f79b7e0a15a8ea3d74e8d1f1a&limit=${limit}&offset=${offset}`;
+    return this.http.get<allCharactersResponse>(endpoint).pipe(debounceTime(300), retry(1),
       map((p) => (p.data.results))
       , catchError(this.handleError));
   }
